349ff6f9ccf0019abac80f9e223cd1c9ee20a1cf
Jaron Heard <jaronheard@gmail.com>
2019-07-07 14:52:32 -0700

    🕺

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="Jaron Heard"
+          title="🕺"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         {allPosts.map(({ node }) => {
