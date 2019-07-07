0fe1441dcc87c1fc939cc875e05e9eff29ad3810
Jaron Heard <jaronheard@gmail.com>
2019-07-07 14:39:02 -0700

    🛹

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,6 +102,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-favicon`,    
     {
       resolve: `gatsby-plugin-typography`,
       options: {
